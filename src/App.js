40cf88c874e821f289590a07ed0a380edc9f8f58
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2020-01-12 12:08:43 -0300

    :beer: Setting routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Olaaa</h1>
-    </div>
+    <BrowserRouter>
+      {/** <Header /> */}
+      <Routes />
+    </BrowserRouter>
   );
 }
 
