7394af3f86138ef3b05ed626fc40476465121788
aryclenio <aryclenio2010@gmail.com>
2020-01-27 15:23:20 -0300

    :bug: Refatorando actions

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,7 +3,14 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const SagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const SagaMiddleware = createSagaMiddleware({
+  sagaMonitor,
+});
 const enhancer =
   process.env.NODE_ENV === 'development'
     ? compose(console.tron.createEnhancer(), applyMiddleware(SagaMiddleware))
--- src/redux/modules/cart/sagas.js
+++ src/redux/modules/cart/sagas.js
@@ -1,10 +1,24 @@
 import api from '../../../services/api';
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addToCartSuccess } from './actions';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { addToCartSuccess, updateAmount } from './actions';
+import { formatPrice } from '../../../util/format';
 //GENERATOR - Se assemelha a async await
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
-  yield put(addToCartSuccess(response.data));
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([
