7b6f3d99037bb3626e41348baff1cd148ddd80b5
aryclenio <aryclenio2010@gmail.com>
2020-01-30 19:25:57 -0300

    :lipstick: Adding hooks integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'react/jsx-filename-extension': [
       'warn',
@@ -27,5 +27,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-param-reassign': off,
     'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exaustive-deps': 'warn',
   },
 };
